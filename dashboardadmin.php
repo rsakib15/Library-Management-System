6c4ca9c61583fce4d1fd56d92d1e891f3352421a
Sakib Rahman <Sakib Rahman>
2017-04-27 22:07:03 +0600

    Add User Panel

---
+++ dashboardadmin.php
@@ -0,0 +1,81 @@
+<?php
+session_start();
+ob_start();
+if(empty($_SESSION["user"])){
+    header("location: index.php");
+}
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+<div class="header-wrapper">
+    <header>
+        <div class="logo">
+            <h1><a href="#">Library Management</a></h1>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="controller/logout_controller.php">Logout</a></li>
+            </ul>
+        </nav>
+    </header>
+</div>
+<?php include_once ('includes/sidebar.php'); ?>
+
+<div class="main-container">
+    <h1>Today's Transaction</h1>
+    <div class="transaction today">
+        <ul>
+            <li>
+                <p><a href="">Reserved Transaction</a></p>
+                <span>7</span>
+            </li>
+            <li>
+                <p><a href="">Borrowed Transaction</a></p>
+                <span>7</span>
+            </li>
+            <li>
+                <p><a href="">Returned Transaction</a></p>
+                <span>7</span>
+            </li>
+            <li>
+                <p><a href="">Violation</a></p>
+                <span>7</span>
+            </li>
+        </ul>
+    </div>
+    <h1>All Transaction</h1>
+    <div class="transaction all">
+        <ul>
+            <li>
+                <p><a href="">Reserved Transaction</a></p>
+                <span>7</span>
+            </li>
+            <li>
+                <p><a href="">Borrowed Transaction</a></p>
+                <span>7</span>
+            </li>
+            <li>
+                <p><a href="">Returned Transaction</a></p>
+                <span>7</span>
+            </li>
+            <li>
+                <p><a href="">Violation</a></p>
+                <span>7</span>
+            </li>
+        </ul>
+    </div>
+</div>
+
+</body>
+</html>
---
+++ model/book_model.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rsaki
+ * Date: 27-Apr-17
+ * Time: 10:06 PM
+ */
